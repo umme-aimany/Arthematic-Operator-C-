19ec475803332fec3a5bfa89d77af9b20cf3fec3
Umme Aiman <122363026+umme-aimany@users.noreply.github.com>
2024-01-10 22:54:04 +0500

    Add files via upload

---
+++ ARTHEMATIC-OPERATORS-C++/index.cpp
@@ -0,0 +1,55 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+
+int main() {
+    // Write C++ code here
+    //////ADDITION OPERATOR (+)///////
+    int a=5;
+    int b=2;
+    std::cout << a+b ;
+     std::cout << " This is ADDITION .\n";
+    ///////SUBTRACTION OPERATOR(-) 
+    int c=9;
+    int d=12;
+    std::cout << c-d;
+     std::cout << " This is SUBTRACTION.\n";
+    
+    /////MULTIPICATION//////
+    int e=5;
+    int f=9;
+    std::cout<<e*f;
+     std::cout << " This is MULTIPICATION.\n";
+    //////DIVISION///////////
+    int g=5;
+    int h=2;
+    std::cout<<g/h;
+     std::cout << " This is division.\n";
+     //////REMINDER-MODULUS-MODE//////
+     int i=5;
+    int j=2;
+    std::cout<<i%j;
+     std::cout << " This is MODULUS.\n";
+     //////RELATIONAL OPERATOR EXAMPLE//////////////
+     int CGPA=4;
+     if(CGPA>4)
+     std::cout<<"you are eligible to scholarship";
+     ////////LOGICAL OPERATOR//////////
+     //////AND OPERATOR(&&)/////////
+     int CGPAS=4;
+     int age=19;
+     if (CGPAS>=4 && age<=20)
+    std:: cout<<"you are eligible ";
+    /////BOTH STATEMENTS ARE TRUE SO CODE WILL RUN
+    
+     //////OR OPERATOR(||)/////////
+     int CGP=2;
+     int ages=19;
+     if (CGP>=4 || ages<=20)
+    std:: cout<<"you are enroll  ";
+    /////if one statement is true  SO CODE WILL RUN///////
+    
+     
+     
+
+    return 0;
+}
